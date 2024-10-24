1218c55eafa2bd98bc3be177dc7c3033e7d5a0f9
jc sarmiento <jacinto011200@gmail.com>
2024-10-24 11:18:44 +0800

    add show password and old data of the register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,84 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>T-Shirt</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" >
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arvo" >
-
-    <style>
-        .page_404 {
-            padding: 40px 0;
-            background: #fff;
-            font-family: 'Arvo', serif;
-        }
-
-        .page_404 img {
-            width: 100%;
-        }
-
-        .four_zero_four_bg {
-
-            background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
-            height: 400px;
-            background-position: center;
-        }
-
-        .four_zero_four_bg h1 {
-            font-size: 80px;
-        }
-
-        .four_zero_four_bg h3 {
-            font-size: 80px;
-        }
-
-        .link_404 {
-            color: #fff !important;
-            padding: 10px 20px;
-            background: #39ac31;
-            margin: 20px 0;
-            display: inline-block;
-        }
-
-        .contant_box_404 {
-            margin-top: -50px;
-        }
-    </style>
-</head>
-
-<body>
-    <section class="page_404">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 ">
-                    <div class="col-sm-10 col-sm-offset-1  text-center">
-                        <div class="four_zero_four_bg">
-                            <h1 class="text-center ">404</h1>
-
-                        </div>
-
-                        <div class="contant_box_404">
-                            <h3 class="h2">
-                                Look like you're lost
-                            </h3>
-
-                            <p>the page you are looking for not avaible!</p>
-
-                            <a href="{{route('dashboard')}}" class="link_404">Home</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
-
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -109,53 +28,69 @@
                             <h1 class="text-2xl font-semibold text-gray-900 mb-4">Register</h1>
                         </div>
                         <form action="{{ route('register.save') }}" method="POST" class="space-y-4">
-                            @csrf
-                            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4"> <!-- Grid for two columns -->
-                                <div>
+                        @csrf
+                            <div class="flex flex-wrap gap-4">
+                                <div class="w-full">
                                     <label for="name" class="block text-sm font-medium text-gray-700">Full Name</label>
-                                    <input name="name" type="text" id="name" required placeholder="Full Name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('name') border-red-500 @enderror">
+                                    <input name="name" type="text" id="name" required placeholder="Full Name" 
+                                        value="{{ old('name') }}" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
+                                        focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('name') border-red-500 @enderror">
                                     @error('name')
                                         <span class="text-red-500 text-sm">{{ $message }}</span>
                                     @enderror
                                 </div>
 
-                                <div>
+                                <div class="w-full">
                                     <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
-                                    <input name="phone" type="text" id="phone" placeholder="09XXXXXXXXX" oninput="this.value = this.value.replace(/[^0-9]/g, '')" required class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('phone') border-red-500 @enderror">
+                                    <input name="phone" type="text" id="phone" placeholder="09XXXXXXXXX" oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0,11)" 
+                                        maxlength="11" required value="{{ old('phone') }}" 
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('phone') border-red-500 @enderror">
                                     @error('phone')
                                         <span class="text-red-500 text-sm">{{ $message }}</span>
                                     @enderror
                                 </div>
 
-                                <div>
-                                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                                    <input name="password" type="password" id="password" required placeholder="Password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('password') border-red-500 @enderror">
-                                    <div class="text-xs text-gray-500 italic">Must be 8-20 characters long, include at least 1 number and both upper and lower case letters.</div>
-                                    @error('password')
-                                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                
-                                <div>
+                                <div class="w-full">
                                     <label for="email" class="block text-sm font-medium text-gray-700">Email Address</label>
-                                    <input name="email" type="email" id="email" required placeholder="Email Address" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('email') border-red-500 @enderror">
+                                    <input name="email" type="email" id="email" required placeholder="Email Address" 
+                                        value="{{ old('email') }}" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
+                                        focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('email') border-red-500 @enderror">
                                     @error('email')
                                         <span class="text-red-500 text-sm">{{ $message }}</span>
                                     @enderror
                                 </div>
 
-                                <div>
+                                <div class="w-full">
+                                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                                    <input name="password" type="password" id="password" required placeholder="Password" 
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
+                                        focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('password') border-red-500 @enderror">
+                                    <div class="text-xs text-gray-500 italic">Must be 8-20 characters long, include at least 1 number and both upper and lower case letters.</div>
+                                    @error('password')
+                                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="w-full">
                                     <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-                                    <input name="password_confirmation" type="password" id="password_confirmation" placeholder="Confirm Password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('password_confirmation') border-red-500 @enderror">
+                                    <input name="password_confirmation" type="password" id="password_confirmation" placeholder="Confirm Password" 
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
+                                        focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('password_confirmation') border-red-500 @enderror">
                                     @error('password_confirmation')
                                         <span class="text-red-500 text-sm">{{ $message }}</span>
                                     @enderror
+                                </div>
+
+                                <div class="flex items-center justify-center">
+                                    <input type="checkbox" id="show-password" class="h-4 w-4">
+                                    <label for="show-password" class="ml-2 text-sm text-gray-700">Show Password</label>
                                 </div>
                             </div>
 
                             <div>
                                 <label for="description" class="block text-sm font-medium text-gray-700">Tell About Yourself</label>
-                                <textarea name="description" id="description" required placeholder="Description" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('description') border-red-500 @enderror"></textarea>
+                                <textarea name="description" id="description" required placeholder="Description" 
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error('description') border-red-500 @enderror">{{ old('description') }}</textarea>
                                 @error('description')
                                     <span class="text-red-500 text-sm">{{ $message }}</span>
                                 @enderror
@@ -169,6 +104,7 @@
                                 Register Account
                             </button>
                         </form>
+
                         <div class="text-center mt-4">
                             <a class="text-sm text-indigo-600 hover:text-indigo-700" href="login">Already registered to an existing account?</a>
                         </div>
@@ -178,6 +114,19 @@
             </div>
         </div>
     </div>
+
+<!-- Show Password -->
+<script>
+    const showPasswordCheckbox = document.getElementById('show-password');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('password_confirmation');
+
+    showPasswordCheckbox.addEventListener('change', function () {
+        const passwordType = this.checked ? 'text' : 'password';
+        passwordInput.type = passwordType;
+        confirmPasswordInput.type = passwordType;
+    });
+</script>
 </body>
 </html>
 
